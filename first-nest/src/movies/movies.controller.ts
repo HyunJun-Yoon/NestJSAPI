0b447c619b6ab289f27d4886ec966c27d30bf3b7
Hyunjun Yoon <yhj940719@gmail.com>
2021-05-19 15:39:50 -0400

    Testing Completed

--- first-nest/src/movies/movies.controller.ts
+++ first-nest/src/movies/movies.controller.ts
@@ -6,11 +6,9 @@ import {
   Param,
   Patch,
   Post,
-  Put,
-  Query,
 } from '@nestjs/common';
-import { CreateMovieDto } from 'src/dto/create-movie.dto';
-import { UpdateMovieDto } from 'src/dto/update-movie.dto';
+import { CreateMovieDto } from '../dto/create-movie.dto';
+import { UpdateMovieDto } from '../dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
